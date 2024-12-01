d79479f4678f3a8b76ecea90197c57256ebeebdb
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-12-01 08:51:27 +0600

    link add

--- app/dashboard/_components/Sidebar.jsx
+++ app/dashboard/_components/Sidebar.jsx
@@ -1,34 +1,58 @@
-import { FaPix } from "react-icons/fa6";
+import Link from "next/link";
 import { FaUniversity } from "react-icons/fa";
-import { TiMessages } from "react-icons/ti";
-import { FaUserGroup } from "react-icons/fa6";
-import { FaBuildingUser } from "react-icons/fa6";
+import {
+  FaBuildingUser,
+  FaHeadphonesSimple,
+  FaPix,
+  FaUserGroup,
+} from "react-icons/fa6";
 import { IoSettingsOutline } from "react-icons/io5";
-import { FaHeadphonesSimple } from "react-icons/fa6";
+import { TiMessages } from "react-icons/ti";
 const DashboardSidebar = () => {
   return (
     <ul className='mt-4 text-white'>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <FaPix /> <span>Dashboard</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <FaUniversity /> <span>Universities</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <TiMessages /> <span>Packages</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <FaUserGroup /> <span>Agents</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <FaBuildingUser /> <span>Students</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <IoSettingsOutline /> <span>Settings</span>
-      </li>
-      <li className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'>
+      </Link>
+      <Link
+        href='#'
+        className='py-2 px-4 hover:text-[#092A67] hover:bg-lime-500 flex justify-start items-center gap-2 cursor-pointer duration-300'
+      >
         <FaHeadphonesSimple /> <span>Contact Us</span>
-      </li>
+      </Link>
     </ul>
   );
 };
